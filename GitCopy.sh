1cfef7b68105028f622fdd025ced96e222615026
Adam <70795583+AdamM-AU@users.noreply.github.com>
2020-10-10 21:42:34 +1100

    Create GitCopy.sh
    #GitCopy.sh - Simple Tool for Cloning a Repo will Full Commit History
    
    #Adam M (S3875753@student.rmit.edu.au)

---
+++ GitCopy.sh
@@ -0,0 +1,51 @@
+#GitCopy.sh - Simple Tool for Cloning a Repo will Full Commit History
+#Adam M (S3875753@student.rmit.edu.au)
+
+echo "Full Repo Clone with Commit Histroy!"
+if [ -z ${1+x} ]
+then
+        echo "No Origin/Source Repo!"
+        echo "GitCopy.sh <Origin/Source> <Destination/Target>"
+        exit
+fi
+
+if [ -z ${2+x} ]
+then
+        echo "No Destination/Target Repo!"
+        exit
+fi
+
+echo "Target: $1"
+echo "Destination: $2"
+mkdir GitCopy-Temp
+cd GitCopy-Temp
+
+RepoDir=${1##*/}
+RepoDir=${RepoDir%".git"}
+
+#Origin/Source Repo
+git clone $1
+cd $RepoDir
+git remote rm origin
+git filter-branch --subdirectory-filter ./ -- --all
+git add .
+git commit
+cd ..
+mv $RepoDir $RepoDir"-Source"
+#End of Origin/Source Repo
+
+
+RepoDir2=${2##*/}
+RepoDir2=${RepoDir2%".git"}
+
+#Target
+git clone $2
+cd $RepoDir2
+git remote add origin-repo ../$RepoDir"-Source"
+git pull origin-repo master
+git remote rm origin-repo
+git push
+
+echo " "
+echo "All Tasks Completed!"
+echo "You may now delete Directory GitCopy-Temp"
